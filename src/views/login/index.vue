460b3d016b0e596dbc871c244b3ae002a07b3ebf
Yiğit Tosun <yigit-tosun@outlook.com>
2023-10-19 23:29:24 +0300

    Edit UI style in login page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-flex justify-content-center align-items-center min-vh-100">
-    <div class="card w-25">
+    <div class="card wider-card">
       <div class="card-body">
-        <h2 class="card-title my-3">Welcome</h2>
+        <h2 class="card-title my-3 text-navy">Welcome</h2>
         <form @submit.prevent="login">
           <div class="mb-3 text-start px-3">
             <label for="email" class="form-label">Email:</label>
@@ -16,7 +16,12 @@
           </div>
           <div class="mb-3 text-start px-3">
             <label for="password" class="form-label">Password:</label>
-            <span data-bs-toggle="tooltip" data-bs-placement="top" title="First 3 numbers">
+            <span
+              class="ms-1"
+              data-bs-toggle="tooltip"
+              data-bs-placement="top"
+              title="First 3 numbers"
+            >
               <i class="bi bi-info-circle h6"></i>
             </span>
             <input
