6bb6efc4d2819a714611a05404b3ce21d8f7ac6e
Yiğit Tosun <yigit-tosun@outlook.com>
2023-10-21 16:52:19 +0300

    Implement firebase api & create upload video function in record preview modal

--- src/components/modals/RecordPreviewModal.vue
+++ src/components/modals/RecordPreviewModal.vue
@@ -24,7 +24,12 @@
 
         <div class="modal-footer">
           <ButtonComponent btnClass="btn-navy" buttonText="Rerecord" data-bs-dismiss="modal" />
-          <ButtonComponent btnClass="btn-success" buttonText="Save" @click="downloadRecordedVideo" />
+          <ButtonComponent
+            btnClass="btn-success"
+            buttonText="Dowload"
+            @click="downloadRecordedVideo"
+          />
+          <ButtonComponent btnClass="btn-success" buttonText="Upload" @click="uploadVideo" />
         </div>
       </div>
     </div>
@@ -32,8 +37,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue'
+import { defineComponent, ref, onMounted, inject } from 'vue'
 import ButtonComponent from '@/components/ButtonComponent.vue'
+import { getStorage, ref as storageRef, uploadBytes } from 'firebase/storage'
+import Swal from 'sweetalert2'
+import { type FirebaseApp } from 'firebase/app'
 
 export default defineComponent({
   name: 'record-preview-modal',
@@ -49,29 +57,57 @@ export default defineComponent({
   setup(props) {
     const recordPreviewModalRef = ref<HTMLElement | null>(null)
 
+    // create app variable to access Firebase Storage
+    const app = inject<FirebaseApp>('firebaseApp')
+    // Upload a video to Firebase Storage
+    const uploadVideo = async () => {
+      const storage = getStorage(app)
+      const videoRef = storageRef(storage, 'videos/recorded-video.webm')
+      const response = await fetch(props.recordedVideoUrl)
+      const blob = await response.blob()
+      await uploadBytes(videoRef, blob)
+        .then(() => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Video Uploaded Successfully',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        })
+        .catch((error) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong...It may be a server related problem. Try again!',
+            footer: `<p>${error}</p>`
+          })
+        })
+    }
+
     const downloadRecordedVideo = () => {
-      const a = document.createElement('a');
-      a.href = props.recordedVideoUrl;
-      a.download = 'recorded-video.webm';
-      a.style.display = 'none';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-    };
+      const a = document.createElement('a')
+      a.href = props.recordedVideoUrl
+      a.download = 'my-recorded-video.webm'
+      a.style.display = 'none'
+      document.body.appendChild(a)
+      a.click()
+      document.body.removeChild(a)
+    }
 
     onMounted(() => {
       if (recordPreviewModalRef.value) {
         recordPreviewModalRef.value.addEventListener('shown.bs.modal', () => {
-          console.log('recordedVideoUrl: ', props.recordedVideoUrl);
-        });
+          console.log('recordedVideoUrl: ', props.recordedVideoUrl)
+        })
       }
-    });
+    })
 
     return {
       recordPreviewModalRef,
+      app,
+      uploadVideo,
       downloadRecordedVideo
     }
   }
 })
 </script>
-
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,31 @@ import "bootstrap"
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { initializeApp } from 'firebase/app';
+import type { FirebaseApp } from 'firebase/app' 
 
 import App from './App.vue'
 import router from './router'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
+const firebaseConfig = {
+    apiKey: 'AIzaSyCl7FNiohbqZBos373K-2vCx25Jv3Lx8mM',
+    authDomain: 'video-challenge-dde63.firebaseapp.com',
+    projectId: 'video-challenge-dde63',
+    storageBucket: 'video-challenge-dde63.appspot.com',
+    messagingSenderId: '1016243725957',
+    appId: '1:1016243725957:web:95597dbaafbc563669abec',
+    measurementId: 'G-816VRW4DZ9'
+}
+
+const firebaseApp = initializeApp(firebaseConfig);
+
 const app = createApp(App)
 
 app.use(VueSweetalert2);
 app.use(createPinia())
 app.use(router)
+app.provide<FirebaseApp>('firebaseApp', firebaseApp);
 
 app.mount('#app')
